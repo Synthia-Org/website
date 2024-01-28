75705b919f027c954da4eb1866a7e59e140ad0bd
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2024-01-28 10:58:46 +0530

    Created Project Structure

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res) => {
+	res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`);
+});

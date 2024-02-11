d0da000ed329e7fb2b385e64137ba27a2134ff44
IsharaPallawattha <isharapallawattha@gmail.com>
2024-02-12 04:46:35 +0530

    home page

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,19 @@
 // script.js
+
+
+'use strict'
+
+const menuToggle = document.querySelector('.menu-toggle');
+
+
+const navBar = document.querySelector('.navbar');
+
+
+
+// -------- toggle switch --------
+$('.tdnn').click(function () {
+    $("body").toggleClass('light');
+    $(".moon").toggleClass('sun');
+    $(".tdnn").toggleClass('day');
+  });
+  

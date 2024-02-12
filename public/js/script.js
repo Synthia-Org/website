18dd6117bac7c5d218a846d0dff464a097be6fde
bakthi gamage <bdgamage@students.nsbm.ac.lk>
2024-02-12 14:36:58 +0530

    updatejs

--- public/js/script.js
+++ public/js/script.js
@@ -19,4 +19,9 @@ $('.tdnn').click(function () {
   
 
 
+  //-----------resources------------
   
+$('div.card-image').each(function() {
+  random_image_index = Math.floor(images.length * Math.random());
+  $(this).css('background-image', 'url(' + images[random_image_index] + ')');
+});
